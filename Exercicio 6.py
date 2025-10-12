7d35e8694ce27e1d31b7a4e881bdb501c6ba7072
Hugogabrielsilvarodrigues <a69236@alunos.ipb.pt>
2025-10-13 00:05:59 +0100

    completed

--- Exercicio 6.py
+++ Exercicio 6.py
@@ -1,5 +1,20 @@
+def calculus():
+    num1 = int(input())
+    operation= input("fun.(+,-,*,/,Clear,Exit): ")
+    while operation not in ("+","-","/","*","Clear","Exit"):
+        print("insert another function")
+        operation= input("fun.(+,-,*,/,Clear,Exit): ")
+    if operation == ("Clear"):
+        return
+    elif operation == ("Exit"):
+        offbutton == True
+    num2 = int(input())
+    if operation == ("+"): res=num1+num2
+    elif operation == ("-"): res=num1-num2
+    elif operation == ("/"): res=num1/num2
+    elif operation == ("*"): res=num1*num2
+    print ("res: ",res)
 offbutton=False
 print ("\tCalculadora")
-num1 = int(input())
 while offbutton == False:
-    
+    calculus()
